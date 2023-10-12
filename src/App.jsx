6ed2e918de42a91f745303275dafb93b403d8ee2
Ahsan <ahsanhd@outlook.com>
2023-10-12 21:55:35 +0500

    Feature: testing CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="product" element={<Product />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,13 @@
-import Navigation from "../components/Navigation";
+import { Link } from "react-router-dom";
+import Navigation from "../components/PageNav";
 
 function Homepage() {
   return (
     <div>
       <Navigation />
       <h1>Homepage</h1>
+
+      <Link to="/app">Go to the App</Link>
     </div>
   );
 }
