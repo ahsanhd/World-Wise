613e142693ebb062884cf416d8d72efb9f836228
Ahsan <ahsanhd@outlook.com>
2023-10-14 21:58:44 +0500

    feature: Pages formatted with modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
