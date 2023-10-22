d76378ee202ad728954ccdc16e0cd19d06e9bd2c
Ahsan <ahsanhd@outlook.com>
2023-10-22 13:40:56 +0500

    Feature: city list complete. from a fake api (local)

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,7 +1,15 @@
 import styles from "./CityItem.module.css";
 import PropTypes from "prop-types";
 function CityItem({ city }) {
-  return <li>city</li>;
+  const { cityName, emoji, date } = city;
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <p className={styles.date}>{date}</p>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
 }
 
 export default CityItem;
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,9 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
