b73b096ad518338c52ea21cdf703d03b00e41a74
Ahsan <ahsanhd@outlook.com>
2023-11-05 04:41:36 +0500

    Feautre: Login and Logout using Context API and useReducer

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
+
+  const navigate = useNavigate();
+  const { login, isAuth } = useAuth();
+
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    await login(email, password);
+  }
+  useEffect(
+    function () {
+      if (isAuth === true) navigate("/app");
+    },
+    [isAuth, navigate]
+  );
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleLogin}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
